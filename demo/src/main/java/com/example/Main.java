413c07f88d5f15c9f8312781ad172c7589851296
Arthur <arthurono@hotmail.com>
2025-10-10 12:41:25 -0300

    comentei coisas e criei 2 magos

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,7 @@
 package com.example;
 
 
-import com.Menu.ColiseuGUI;
+//import com.Menu.ColiseuGUI;
 import com.GerenciadorDeMagos.Gerenciador;
 import com.Menu.Menu;
 //import com.personagem.MagoElemental;
